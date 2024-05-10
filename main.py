4e0cc414be01e939d7bce28419ca2b6acebcbef8
jpwm2 <kohsaku.sub2@gmail.com>
2024-05-10 17:11:29 +0900

    途中

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import tkinter as tk
 import threading
 import queue
-import time
+import time as sys_time
 import random
 
-class Land:
+class MoneyTree:
+    pass
+
+class Land(MoneyTree):
     harvest_amount = 4
 
-class Machine:
+
+class Machine(MoneyTree):
     production_amount = 1
 
-class Person:
 
-    class Indivisuality:
+class Person(MoneyTree):
+    class Individuality:
         index = 3
         range1 = [1, 10, 100]
         range2 = [1, 100, 10000]
         range3 = [0, 1, 2]
         range4 = [0.5, 0.75, 1]
         range5 = [0, 0.5, 1]
+
         def __init__(self, seed):
-            def determine_active(seed1, seed2, rangeX):
-                value = rangeX[int((seed1/ (self.index**seed2))%self.index)] 
+            def determine_active(seed1, seed2, range_x):
+                value = range_x[int((seed1 / (self.index ** seed2)) % self.index)]
                 return value
-            self.invest_acitive = determine_active(seed, 0, self.range1)
-            self.loan_acitive = determine_active(seed, 1, self.range2)
-            self.baby_acitive = determine_active(seed, 2, self.range3)
+
+            self.invest_active = determine_active(seed, 0, self.range1)
+            self.loan_active = determine_active(seed, 1, self.range2)
+            self.baby_active = determine_active(seed, 2, self.range3)
             self.inheritance_active = determine_active(seed, 3, self.range4)
             self.capital_emphasis = determine_active(seed, 4, self.range5)
 
-
-        
     gold = 10
     land = [Land()]
     food = 10
@@ -38,25 +42,166 @@ class Person:
     product = 0
     utility = 0
     children = []
+    stamina = 1
 
-    def __init__(self, indivisuality_seed):
-        self.indivisuality = self.Indivisuality(indivisuality_seed)
+    def __init__(self, individuality_seed):
+        self.individuality = self.Individuality(individuality_seed)
 
-    def life(self):
+    def get_stamina(self):
+        return self.stamina
+
+class Time:
+    flag = True
+
+    def use(self):
+        self.flag = False
+        return True
+
+class InvestmentExchange:
+    class InvestmentMarket:
+        orders = []
+        capital_yield = 0
+        income_yield = 0
+
+    markets = []
+
+    def __init__(self, number):
+        for i in range(number):
+            self.markets.append(self.InvestmentMarket())
+
+
+class InvestmentSellExchange(InvestmentExchange):
+    def __init__(self, number):
+        super().__init__(number)
+
+
+class InvestmentBuyExchange(InvestmentExchange):
+    def __init__(self, number):
+        super().__init__(number)
+
+
+class LoanExchange:
+    class LoanMarket:
+        orders = []
+        income_yield = 0
+
+    markets = []
+
+    def __init__(self, number):
+        for i in range(number):
+            self.markets.append(self.LoanMarket())
+
+
+class LoanSellExchange(LoanExchange):
+    def __init__(self, number):
+        super().__init__(number)
+
+
+class LoanBuyExchange(LoanExchange):
+    def __init__(self, number):
+        super().__init__(number)
+
+
+class LaborExchange:
+    orders = []
+
+    def get_job(self, stamina):
+        def divide():
+            order.quantity -= stamina
+            return Order(stamina, order.price, order.player)
+
+        jobs = []
+        for order in self.orders.copy():
+            if order.quantity < stamina:
+                jobs.append(order)
+                stamina -= order.quantity
+                self.orders.remove(order)
+            else:
+                divided_order = divide()
+                jobs.append(divided_order)
+
+
+class Order:
+    def __init__(self, quantity, price, player):
+        self.quantity = quantity
+        self.price = price
+        self.player = player
+
+
+class SimulationEnvironment:
+    num_people = 243
+    investment_number = 2
+    loan_number = 1
+
+    def __init__(self):
+        def initialize_people():
+            for i in range(self.num_people):
+                person = Person(i)
+                self.people.append(person)
+            self.shuffled_people = self.people.copy()
+            self.utility_people = self.people.copy()
+            self.money_people = self.people.copy()
+
+        def initialize_exchanges():
+            self.investment_sell_exchange = InvestmentSellExchange(self.investment_number)
+            self.investment_buy_exchange = InvestmentBuyExchange(self.investment_number)
+            self.loan_sell_exchange = LoanSellExchange(self.loan_number)
+            self.loan_buy_exchange = LoanBuyExchange(self.loan_number)
+            self.labor_exchange = LaborExchange()
+
+        self.people = []
+        self.shuffled_people = None
+        self.utility_people = None
+        self.money_people = None
+        self.investment_sell_exchange = None
+        self.investment_buy_exchange = None
+        self.loan_sell_exchange = None
+        self.loan_buy_exchange = None
+        self.labor_exchange = None
+        initialize_people()
+        initialize_exchanges()
+
+    class Workbench:
+        def stamina(self, time):
+            if time.use():
+                return 1
+            else:
+                return 0
+
+class SimulationThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.simulation_value = 0
+        self._stop_event = threading.Event()
+        self.environment = SimulationEnvironment()
+
+    def run(self):
+        while not self._stop_event.is_set():
+            random.shuffle(self.environment.shuffled_people)
+            for person in self.environment.shuffled_people:
+                self.one_day(person)
+
+    def stop(self):
+        self._stop_event.set()
+
+    def get_value(self):
+        return self.environment
+
+    def one_day(self, person):
         def work():
             def find():
-                stamina = 1
-
+                jobs = self.environment.labor_exchange.get_job()
 
             def run():
                 pass
 
             def receive():
                 pass
+
             find()
             run()
             receive()
-            
+
         def eat():
             def find():
                 pass
@@ -66,6 +211,7 @@ class Person:
 
             def run():
                 pass
+
             find()
             pay()
             run()
@@ -79,10 +225,11 @@ class Person:
 
             def receive():
                 pass
+
             judge()
             pay()
             receive()
-        
+
         def play():
             def find():
                 pass
@@ -96,109 +243,11 @@ class Person:
             find()
             pay()
             run()
+
         work()
         eat()
         invest()
         play()
-
-class InvestmentExchange:
-    class InvestmentMarket:
-        orders = []
-        capital_yield = 0
-        income_yield = 0
-    markets = []
-
-    def __init__(self, number):
-        for i in range(number):
-            self.markets.append(self.InvestmentMarket())
-
-class InvestmentSellExchange(InvestmentExchange):
-    def __init__(self, number):
-        super.__init__(number)
-
-class InvestmentBuyExchange(InvestmentExchange):
-    def __init__(self, number):
-        super.__init__(number)
-
-
-class LoanExchange:
-    class LoanMarket:
-        orders = []
-        income_yield = 0
-    markets = []
-
-    def __init__(self, number):
-        for i in range(number):
-            self.markets.append(self.LoanMarket())
-
-class LoanSellExchange(LoanExchange):
-    def __init__(self, number):
-        super.__init__(number)
-
-class LoanBuyExchange(LoanExchange):
-    def __init__(self, number):
-        super.__init__(number)
-
-class LaborExchange:
-    orders = []
-
-    def get_job(self, stamina):
-        jobs = []
-        for order
-
-    
-
-
-class Order:
-    def __init__(self, quantity, price, player):
-        self.quantity = quantity
-        self.price = price
-        self.player = player
-
-class SimulationEnvironment:
-    num_people = 243
-    investment_number = 2
-    loan_number = 1
-
-    def __init__(self):
-        self.initialize_people()
-        self.initialize_exchanges()
-        
-    def initialize_people(self):
-        self.people = []
-        for i in range(self.num_people):
-            person = Person(i)
-            self.people.append(person)
-        self.shuffled_people = self.people.copy()
-        self.utility_people = self.people.copy()
-        self.money_people = self.people.copy()
-
-    def initialize_exchanges(self):
-        self.investment_sell_exhange = InvestmentSellExchange(self.investment_number)
-        self.investment_buy_exchange = InvestmentBuyExchange(self.investment_number)
-        self.loan_sell_exchange = LoanSellExchange(self.loan_number)
-        self.loan_buy_exchange = LoanBuyExchange(self.loan_number)
-        self.labor_exchange = LaborExchange()
-
-
-class SimulationThread(threading.Thread):
-    def __init__(self):
-        super().__init__()
-        self.simulation_value = 0
-        self._stop_event = threading.Event()
-        self.environment = SimulationEnvironment()
-
-    def run(self):
-        while not self._stop_event.is_set():
-            random.shuffle(self.shuffled_people)
-            for person in self.shuffled_people:
-                pass
-
-    def stop(self):
-        self._stop_event.set()
-
-    def get_value(self):
-        return self.environment
 
 
 # GUIの更新をメインスレッドで行う
@@ -214,11 +263,13 @@ def update_gui(root, label, gui_queue):
 
     root.after(100, update_gui, root, label, gui_queue)  # 100msごとにチェック
 
+
 def on_resize(event):
     # ウィンドウのサイズを取得
     new_width = event.width
     new_height = event.height
     print(f"New size: {new_width}x{new_height}")
+
 
 # GUIを管理するスレッド
 class GuiManager:
@@ -245,7 +296,7 @@ class GuiManager:
 
     def update_simulation_value(self):
         while not self.simulation_thread._stop_event.is_set():
-            time.sleep(0.5)  # 500msごとに更新
+            sys_time.sleep(0.5)  # 500msごとに更新
             environment = self.simulation_thread.get_value()
             # キューを使ってメインスレッドに送信
             self.gui_queue.put(environment)
